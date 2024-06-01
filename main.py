3c568ed1817b81e6f9aef86dcb021c544d6b93fb
Jack W Finlay <jack@jackfinlay.com>
2024-06-01 23:04:12 +1000

    Ping roles not working in threads due to size

--- main.py
+++ main.py
@@ -15,29 +15,24 @@ medium_ping = environ["DISCORD_MEDIUM_PING"]
 hard_ping = environ["DISCORD_HARD_PING"]
 start_date = datetime.fromisoformat(environ["STUDY_PLAN_START_DATE"])
 
-difficultyPings = {"Easy": easy_ping, "Medium": medium_ping, "Hard": hard_ping}
+difficulty_pings = {"Easy": easy_ping, "Medium": medium_ping, "Hard": hard_ping}
+difficulty_colors = {"Easy": "🟢", "Medium": "🟡", "Hard": "🔴"}
 
 discord.utils.setup_logging()
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 
 
-def create_message_body():
-    message_body = f"<{url}>"
+def create_message_body(problem_url, problem_difficulty):
+    message_body = f"<{problem_url}>" + "\n" + f"<@&{default_ping}>, <@&{difficulty_pings.get(problem_difficulty)}>"
     print(f"Message body: '{message_body}'")
     return message_body
 
 
-def create_thread_title(problem_number, problem_name):
-    thread_title = f"{problem_number} - {problem_name}"
+def create_thread_title(problem_number, problem_name, problem_difficulty):
+    thread_title = f"{difficulty_colors.get(problem_difficulty)} {problem_number} - {problem_name}"
     print(f"Thread title: '{thread_title}'")
     return thread_title
-
-
-def create_thread_message_body():
-    message_body = f"<@&{default_ping}>, <@&{difficultyPings.get(difficulty)}>"
-    print(f"Thread message body: '{message_body}'")
-    return message_body
 
 
 def calculate_day_number(start):
@@ -59,13 +54,11 @@ difficulty = problem["difficulty"]
 
 
 async def send_message():
-    message_body = create_message_body()
-    thread_title = create_thread_title(day, name)
-    thread_body = create_thread_message_body()
+    message_body = create_message_body(url, difficulty)
+    thread_title = create_thread_title(day, name, difficulty)
     channel = client.get_channel(channel_id)
     message = await channel.send(message_body)
     thread = await message.create_thread(name=thread_title)
-    await thread.send(thread_body)
     await client.close()
 
 
